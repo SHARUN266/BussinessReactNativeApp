3159a36fe87fe4ce2a64f99f192e87741fb64fe1
Sharun 1 <sharun.1>
2024-06-12 21:49:13 +0530

    loading added

--- app/businesslist/[category].js
+++ app/businesslist/[category].js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, ActivityIndicator } from "react-native";
 import React, { useEffect, useState } from "react";
 import { useLocalSearchParams, useNavigation } from "expo-router";
 import { collection, getDocs, query, where } from "firebase/firestore";
@@ -10,44 +10,59 @@ export default function BusinessListByCategory() {
   const navigation = useNavigation();
   const { category } = useLocalSearchParams();
   const [businessList, setBusinessList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     getBusinessList();
     navigation.setOptions({
       headerShown: true,
       headerTitle: category,
     });
- 
   }, []);
- 
+
   const getBusinessList = async () => {
-    
+    setBusinessList([]);
+    setIsLoading(true);
     const q = query(
       collection(db, "BussinessList"),
       where("category", "==", category)
     );
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
-     
-     setBusinessList(prev => [...prev, doc.data()]);
+      setBusinessList((prev) => [...prev, doc.data()]);
     });
+    setIsLoading(false);
   };
- // console.log(businessList)
+  // console.log(businessList)
   return (
     <View>
-      {
-        businessList?.length>0?
+      {businessList?.length > 0 && isLoading == false ? (
         <FlatList
-        data={businessList}
-        renderItem={({ item, index }) => (
-          <BusinessListCard business={item} key={index} />
-  )}
-      />:
-      <Text style={{fontSize:20,fontFamily:'outfit-bold',color:Colors.GRAY,textAlign:'center',marginTop:'40%'}}>
-        No Business Found!
-      </Text>
-
-      }
-     
+          onRefresh={getBusinessList}
+          refreshing={isLoading}
+          data={businessList}
+          renderItem={({ item, index }) => (
+            <BusinessListCard business={item} key={index} />
+          )}
+        />
+      ) : isLoading ? (
+        <ActivityIndicator
+          style={{ marginTop: "60%" }}
+          size={"large"}
+          color={Colors.PRIMARY}
+        />
+      ) : (
+        <Text
+          style={{
+            fontSize: 20,
+            fontFamily: "outfit-bold",
+            color: Colors.GRAY,
+            textAlign: "center",
+            marginTop: "40%",
+          }}
+        >
+          No Business Found!
+        </Text>
+      )}
     </View>
   );
 }
